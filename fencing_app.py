e28c66c92a2f8bbe2e794497497f26a451f9f5d1
tom-xf <tom@x-functions.com>
2022-07-19 13:43:13 +1000

    vega chart

--- fencing_app.py
+++ fencing_app.py
@@ -141,4 +141,17 @@ if len(fencers) > 0:
 
   df3 = df2.groupby('Date').count()['Name']
   if df3.shape[0]>0:
-    st.line_chart(df3)
+    st.vega_lite_chart(df3, {
+
+    })
+
+
+
+st.vega_lite_chart(df, {
+     'mark': 'bar',
+     'encoding': {
+         'x': {'field': 'Date', 'type': 'nominal'},
+         'y': {'field': 'Name', 'type': 'quantitative'},
+         'color': '#0CF66A',
+     },
+ })
