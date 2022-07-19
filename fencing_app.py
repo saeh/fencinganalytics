d2eb2603f178d82019e8cb707643257cf5debb5b
tom-xf <tom@x-functions.com>
2022-07-19 13:49:30 +1000

    fix colour

--- fencing_app.py
+++ fencing_app.py
@@ -144,10 +144,12 @@ if len(fencers) > 0:
     st.table(df3)
     st.vega_lite_chart(df3, {
         "width": "container",
-        'mark': 'bar',
+        'mark': {
+          'type': 'bar',
+          'color': '#0CF66A',
+        },
         "encoding": {
           "x": {"field": "Date", "type": "nominal", "axis": {"labelAngle": 0}},
           "y": {"field": "Name", "type": "quantitative"},
-          "color": '#0CF66A',
         }
     })
